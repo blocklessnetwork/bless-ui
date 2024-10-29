e8b7a31555b76e1bd260ea25ad8202e76efb5c85
Flavio Espinoza <flavio.espinoza@gmail.com>
2024-10-29 11:30:38 -0600

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function HomePage() {
+	return (
+		<div className="flex-1 mt-12 mb-12 relative">
+			<div className="mx-auto container">Home Page</div>
+		</div>
+	)
+}
