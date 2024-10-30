9020ce0c180d1540be131dcf8e2494214ab33a7c
Flavio Espinoza <flavio.espinoza@gmail.com>
2024-10-30 08:38:29 -0600

    Added link button to input.

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -5,18 +5,19 @@ import * as React from 'react'
 export default function GetStartedPage() {
 	return (
 		<div className="flex-1 items-center justify-items-center p-12 bg-slate">
-			<div className="p-8 mb-12 bg-slate w-[600px] h-[264px] rounded-60 drop-shadow-main">
-				<p className="text-2xl text-charcoal">
+			<div className="p-6 mb-12 bg-slate w-[600px] h-[264px] rounded-60 drop-shadow-main">
+				<p className="text-2xl text-charcoal px-4">
 					Contribute your unused computer power, earn rewards effortlessly.{' '}
 					<Link href="https://chromewebstore.google.com/category/extensions" target="_blank">
 						Get the chrome extension.
 					</Link>
 				</p>
 			</div>
-			<div className="m-auto p-8 bg-slate w-[600px] h-[200px] rounded-60 drop-shadow-main">
+			<div className="p-6 bg-slate w-[600px] h-[200px] rounded-60 drop-shadow-main">
 				<div className="relative h-full">
-					<p className="text-2xl text-charcoal">Full Name</p>
+					<p className="text-2xl text-charcoal px-4">Full Name</p>
 					<Input className="absolute bottom-0" placeholder="Email" />
+					<Link className="absolute right-10 bottom-6" href="/">SIGN UP</Link>
 				</div>
 			</div>
 		</div>
