9ae2d2724d45d819b27015816820a3420fba6c5f
Flavio Espinoza <flavio.espinoza@gmail.com>
2024-10-30 10:21:52 -0600

    Added Card component.

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from '@/components/ui/card'
 import Input from '@/components/ui/input'
 import Link from 'next/link'
 import * as React from 'react'
@@ -5,21 +6,23 @@ import * as React from 'react'
 export default function GetStartedPage() {
 	return (
 		<div className="flex-1 items-center justify-items-center p-12 bg-slate">
-			<div className="p-6 mb-12 bg-slate w-[600px] h-[264px] rounded-60 drop-shadow-main">
+			<Card className="w-[600px] h-[264px]">
 				<p className="text-2xl text-charcoal px-4">
 					Contribute your unused computer power, earn rewards effortlessly.{' '}
 					<Link href="https://chromewebstore.google.com/category/extensions" target="_blank">
 						Get the chrome extension.
 					</Link>
 				</p>
-			</div>
-			<div className="p-6 bg-slate w-[600px] h-[200px] rounded-60 drop-shadow-main">
+			</Card>
+			<Card className="w-[600px] h-[200px]">
 				<div className="relative h-full">
 					<p className="text-2xl text-charcoal px-4">Full Name</p>
 					<Input className="absolute bottom-0" placeholder="Email" />
-					<Link className="absolute right-10 bottom-6" href="/">SIGN UP</Link>
+					<Link className="absolute right-10 bottom-6" href="/">
+						SIGN UP
+					</Link>
 				</div>
-			</div>
+			</Card>
 		</div>
 	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 import './globals.css'
 
-export default function Landing() {
+export default function LandingPage() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-light">
 			<main className="flex flex-col gap-4 row-start-2 items-center">
